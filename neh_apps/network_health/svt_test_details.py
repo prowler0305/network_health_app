94d58369be8929ed57552a08f17feeb3a10ba505
aspea002 <Andrew.Spear@uscellular.com>
2018-11-15 19:23:03 -0600

    Add feature to click on a SVT status cell to see details.
    On the dashboard the individual SVT status cells will be clicked and be
    directed to a new page that will call an API to get details of the SVT
    tests for the last 4 hours.

--- neh_apps/network_health/svt_test_details.py
+++ neh_apps/network_health/svt_test_details.py
@@ -12,7 +12,8 @@ import time
 
 class SvtDetails(MethodView):
     def __init__(self):
-        tems_svt_api_base = "http://sa521-iv-result-api-sa3-auto-test.engapps.uscc.com/iv_results_api/tc/"
+        self.tems_svt_api_base = "http://sa521-iv-result-api-sa3-auto-test.engapps.uscc.com/iv_results_api/tc/"
+        self.pcap_ftp = 'ftp://ascomftp:ascomftp123@'
 
     def get(self):
         """
@@ -20,32 +21,32 @@ class SvtDetails(MethodView):
         :return: Renders the html page with all substituted content needed.
         """
         requesting_test_case = request.args.get('test_case')[0:6]
-        lcc_site = request.args.get('lcc_location')
+        lcc_site = request.args.get('lcc')
+        lcc_code = None
 
-        # TODO: insert call to API here later
+        lcc_list_resp = requests.get(self.tems_svt_api_base + '/lcc/list')
+        if lcc_list_resp.status_code == requests.codes.ok:
+            for lcc_location_item in lcc_list_resp.json():
+                if lcc_location_item.get('lcc_name').lower() == lcc_site:
+                    lcc_code = lcc_location_item.get('lcc_abbrv')
+                    lcc_test_details_resp = requests.get(self.tems_svt_api_base + '/tc/' + requesting_test_case + '/' +
+                                                         lcc_code)
+                    if lcc_test_details_resp.status_code == requests.codes.ok:
+                        lcc_details_dict = lcc_test_details_resp.json()
+                        lcc_details_result_list = lcc_details_dict.get('results')
 
-        with open("C:\\Users\Owner\IdeaProjects\\network_health_app\\neh_apps\\network_health\\svt_test_details.py") as fh:
-            svt_details_dict = json.load(fh)
+                        for details_dict in lcc_details_result_list:
+                            if 'UEA_PCAPUploadFile' in details_dict:
+                                if details_dict.get('UEA_PCAPUploadFile') is not None:
+                                    details_dict['UEA_PCAPUploadFile'] = self.pcap_ftp + \
+                                                                         details_dict.get('UEA_PCAPUploadFile')
+                                if 'UEB_PCAPUploadFile' in details_dict:
+                                    if details_dict.get('UEB_PCAPUploadFile') is not None:
+                                        details_dict['UEB_PCAPUploadFile'] = self.pcap_ftp + \
+                                                                             details_dict.get('UEB_PCAPUploadFile')
+                            for key, value in details_dict.items():
+                                if value is None:
+                                    details_dict[key] = ""
 
-        print(svt_details_dict)
-        return render_template('network_health/svt_test_details.html')
-
-    def redirect_to_uscc_login(self):
-        """
-        Redirects to the USCC Login application
-        :return: redirect response
-        """
-
-        self.login_redirect_response = redirect(os.environ.get('login_app') + '?referrer=' +
-                                                url_for('ne_text', _external=True))
-        return
-
-    @staticmethod
-    def data_file_to_dict(file_path):
-        """
-
-        :param file_path:
-        :return:
-        """
-        with open(file_path) as fh:
-            return json.load(fh)
+        return render_template('network_health/svt_test_details.html', svt_list_tc_details=lcc_details_result_list,
+                               tc_name=lcc_details_dict.get('tc_name'))
